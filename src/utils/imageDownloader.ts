b47339e2ec37b25b3c49204342beea6cf1e31eb6
Ayush Kumar <ayushnbt94@gmail.com>
2026-07-19 14:21:28 +0530

    fix: import expo-media-library from legacy to silence sdk warnings

--- src/utils/imageDownloader.ts
+++ src/utils/imageDownloader.ts
@@ -9,7 +9,7 @@
  */
 
 import * as FileSystem from 'expo-file-system/legacy';
-import * as MediaLibrary from 'expo-media-library';
+import * as MediaLibrary from 'expo-media-library/legacy';
 
 // ---------------------------------------------------------------------------
 // Types
